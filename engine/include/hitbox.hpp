d2e96b024bdece2b4428b7863d2f14de727c81df
Luan Guimarães <livreluan@gmail.com>
2017-06-02 11:09:56 -0300

    Refactoring hitbox

--- engine/include/hitbox.hpp
+++ engine/include/hitbox.hpp
@@ -14,17 +14,22 @@ namespace engine{
       SDL_Rect hitbox;
 
     public:
-      Hitbox(SDL_Rect box,
-        std::pair<int, int> placement,
-        bool active)
-        :hitbox(box),
-        Component("hitbox", placement, active, 5){};
+      Hitbox(
+        std::string name,
+        std::pair<int, int> coordinates,
+        std::pair<int, int> dimensions,
+        bool active = true,
+        int priority = 5)
+        :hitbox({coordinates.first, coordinates.second,
+                         dimensions.first, dimensions.second}),
+        Component(name, coordinates, active, priority){};
 
       ~Hitbox(){};
 
-      SDL_Rect get_hitbox();
-      void set_hitbox(std::pair<int, int>, std::pair<int, int>);
-
+      std::pair<int, int> get_dimensions();
+      void set_dimensions(std::pair<int, int>);
+      bool collides_with(Hitbox*);
+      void set_coordinates(std::pair<int, int>);
   };
 }
 
--- engine/src/hitbox.cpp
+++ engine/src/hitbox.cpp
@@ -1,19 +1,55 @@
 #include <string>
 #include <iostream>
 
-#include "../include/hitbox.hpp"
+#include "hitbox.hpp"
 
 using namespace engine;
 
-SDL_Rect Hitbox::get_hitbox(){
-  return hitbox;
+
+std::pair<int, int> Hitbox::get_dimensions(){
+  return std::make_pair(hitbox.w, hitbox.h);
 }
 
-void Hitbox::set_hitbox(std::pair<int, int> xy, std::pair<int, int> widthheight){
-    hitbox = {
-      .x = xy.first + displacement.first,
-      .y = xy.second + displacement.second,
-      .w = widthheight.first,
-      .h = widthheight.second
-    };
+void Hitbox::set_dimensions(std::pair<int, int> p_dimensions){
+  hitbox.w = p_dimensions.first;
+  hitbox.h = p_dimensions.second;
+}
+
+void set_coordinates(std::pair<int, int> p_coordinates){
+  hitbox.x = p_coordinates.first;
+  hitbox.y = p_coordinates.second;
+  Component::set_coordinates(p_coordinates);
+}
+
+bool Hitbox::collides_with(Hitbox* other_hitbox){
+  std::pair<int, int> other_hitbox_coordinates;
+  int left_a, left_b;
+  int right_a, right_b;
+  int top_a, top_b;
+  int bottom_a, bottom_b;
+
+  left_a = hitbox.x;
+  right_a = hitbox.x + hitbox.w;
+  top_a = hitbox.y;
+  bottom_a = hitbox.y + hitbox.h;
+
+  other_hitbox_coordinates = other->get_coordinates();
+  other_hitbox_dimensions = other->get_dimensions();
+
+  left_b = other_hitbox_coordinates.first;     //x
+  right_b = other_hitbox_coordinates.first +   //x
+            other_hitbox_dimensions.first;     //width
+  top_b = other_hitbox_coordinates.second;     //y
+  bottom_b = other_hitbox_coordinates.second + //y
+             other_hitbox_dimensions.second;   //height
+
+  if(bottom_a <= top_b)
+    return false;
+  if(top_a >= bottom_b)
+    return false;
+  if(right_a <= left_b)
+    return false;
+  if(left_a >= right_b)
+    return false;
+  return true;
 }
