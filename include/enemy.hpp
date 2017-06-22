e891220c58ccb8143f631c2a75ec8ecaebc07d66
Victor Navarro <victor.matias.navarro@gmail.com>
2017-06-22 11:20:02 -0300

    Creating combat interface

--- include/enemy.hpp
+++ include/enemy.hpp
@@ -4,13 +4,14 @@
 #include "../engine/include/game_object.hpp"
 #include "../engine/include/keyboard_event.hpp"
 #include "../engine/include/image.hpp"
+#include "fighter.hpp"
 #include <iostream>
 #include <vector>
 #include <string>
 
 namespace mindscape {
 
-  class Enemy : public engine::GameObject {
+  class Enemy : public engine::GameObject, public mindscape::Fighter {
 
     public:
       Enemy(std::string pname, std::pair<int, int> position, int priority): engine::GameObject(pname, position, priority, {{}}){};
