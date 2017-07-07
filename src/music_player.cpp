79d3429221d431dfbfaf9b45a65dc0b9d50f7827
Lucas S <lucas.soaresouza@gmail.com>
2017-07-07 12:31:43 -0300

    Fixing first song of level 2

--- src/music_player.cpp
+++ src/music_player.cpp
@@ -7,8 +7,12 @@ using namespace mindscape;
 
 void MusicPlayer::update_state(){
 
+    //Search and play the song of main_menu
    if(get_audio_by_name("music_menu")){
+
         play_song("music_menu");
+
+    //search and play the songs of level_1
     }else if(get_audio_by_name("intro_level_1")){
         time += timer->time_elapsed() - time_aux;
         time_aux = timer->time_elapsed();
@@ -21,6 +25,11 @@ void MusicPlayer::update_state(){
             free_music("intro_level_1");
             play_song("loop_level_1");
         }
+
+    //search and play the songs of level_2
+    }else if(get_audio_by_name("loop_level_2")){
+        set_music_volume("loop_level_2", 30);
+        play_song("loop_level_2");
 
     }
 
