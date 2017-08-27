a12efb19d2cbd655322505c7ce1084ce09eb2765
guiaugusto <guilherme.francais@gmail.com>
2017-08-27 22:10:15 +0000

    [STYLE SHEET] Applies technique 6
    Signed-off-by: guiaugusto <guilherme.francais@gmail.com>

--- include/background.hpp
+++ include/background.hpp
@@ -13,22 +13,36 @@
 #include <unordered_map>
 
 namespace mindscape {
-  class Background : public engine::GameObject {
-    private:
-      int paralax = 0;
+  
+    class Background : public engine::GameObject {
+        private:
+            int paralax = 0;
 
-    public:
-      Background(
-        std::string name,std::pair<int, int> position, int priority)
-        :engine::GameObject(name, position, priority,
-          {{engine::KeyboardEvent::LEFT,"MOVE_LEFT"},
-           {engine::KeyboardEvent::RIGHT,"MOVE_RIGHT"}}
-        ){};
-      ~Background(){};
-      void on_event(GameEvent game_event);
-      void set_paralax(int);
-      int get_paralax();
-  };
+        public:
+            Background(
+                std::string name,
+                std::pair<int, int> position,
+                int priority
+            )
+            :engine::GameObject(
+                name, 
+                position, 
+                priority,
+                {
+                {engine::KeyboardEvent::LEFT,"MOVE_LEFT"},
+                {engine::KeyboardEvent::RIGHT,"MOVE_RIGHT"}
+                }
+            ){
+            };
+
+            ~Background(){};
+            
+            void on_event(GameEvent game_event);
+            
+            void set_paralax(int);
+            
+            int get_paralax();
+    };
 }
 
 #endif
