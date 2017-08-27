ff1e55ed2e328ae7cf1912348a95e69b463353e5
guiaugusto <guilherme.francais@gmail.com>
2017-08-27 17:52:47 +0000

    [STYLE SHEET] Applies technique 5
    Signed-off-by: guiaugusto <guilherme.francais@gmail.com>

--- src/select_arrow.cpp
+++ src/select_arrow.cpp
@@ -7,104 +7,119 @@
 using namespace mindscape;
 
 SelectArrow::SelectArrow(
-  std::string name,
-  std::pair<int, int> position,
-  int priority)
-  :engine::GameObject(
-    name,
-    position,
-    priority,
-    {
-      {engine::KeyboardEvent::UP,"UP"},
-      {engine::KeyboardEvent::DOWN,"DOWN"},
-      {engine::KeyboardEvent::RETURN,"ENTER"}
-    }){
-
+    std::string name,
+    std::pair<int, int> position,
+    int priority)
+    :engine::GameObject(
+        name,
+        position,
+        priority,
+        {
+            {engine::KeyboardEvent::UP,"UP"},
+            {engine::KeyboardEvent::DOWN,"DOWN"},
+            {engine::KeyboardEvent::RETURN,"ENTER"}
+        }
+    )
+{
   initialize_arrow();
 };
 
-void SelectArrow::initialize_arrow(){
-  engine::Game game = engine::Game::get_instance();
-  engine::Text* sel = new engine::Text(game.get_renderer(), "selector", std::make_pair(0,0), 5, ">", "../assets/fonts/FFF_Tusj.ttf", 35);
-  sel->activate();
-  add_component(sel);
-  timer->init();
+void SelectArrow::initialize_arrow() {
+    engine::Game game = engine::Game::get_instance();
+    
+    engine::Text* sel = new engine::Text(game.get_renderer(),
+    "selector", std::make_pair(0,0), 5, ">",
+    "../assets/fonts/FFF_Tusj.ttf", 35
+    );
+    
+    sel->activate();
+    add_component(sel);
+    timer->init();
 
-  action = new Action(Action::Command::CHANGE_SCENE);
-
-
+    action = new Action(Action::Command::CHANGE_SCENE);
 }
 
-void SelectArrow::on_event(GameEvent game_event){
-  std::string event_name = game_event.game_event_name;
-  time += timer->time_elapsed() -  time_aux;
-  time_aux = timer->time_elapsed();
+void SelectArrow::on_event(GameEvent game_event) {
+    std::string event_name = game_event.game_event_name;
 
-  if(enable){
-    if(event_name == "DOWN"){
-      enable = false;
-      next_time = time + 200;
-      if(arrow_seletor >= 0 && arrow_seletor < 3){
-        arrow_seletor += 1;
-      } else {
-        arrow_seletor = 0;
-      }
+    time += timer->time_elapsed() - time_aux;
+    time_aux = timer->time_elapsed();
+
+    if (enable) {
+        if (event_name == "DOWN") {
+            enable = false;
+            next_time = time + 200;
+            
+            if (arrow_seletor >= 0 && arrow_seletor < 3) {
+                arrow_seletor += 1;
+            } 
+            else {
+                arrow_seletor = 0;
+            }
+        }
+
+        if (event_name == "UP") {
+            enable = false;
+            next_time = time + 200;
+            
+            if (arrow_seletor <= 3 && arrow_seletor > 0) {
+                arrow_seletor -= 1;
+            } 
+            else {
+                arrow_seletor = 3;
+            }
+        }
     }
 
-    if(event_name == "UP"){
-      enable = false;
-      next_time = time + 200;
-      if(arrow_seletor <= 3 && arrow_seletor > 0){
-        arrow_seletor -= 1;
-      } else {
-        arrow_seletor = 3;
-      }
+    if (enable == false) {
+        if (time > next_time) {
+            enable = true;
+            time = 0;
+        }
     }
-   }
-
-   if(enable == false){
-      if(time > next_time){
-        enable = true;
-        time = 0;
-      }
-   }
 
 
-  switch(arrow_seletor){
-    //INICIAR
-    case(0):
-      set_position(std::make_pair(get_position().first, 175));
-      if(event_name == "ENTER"){
-        action->execute("../data/1.level.dat");
-      }
-      break;
-    //INSTRUÇÕES
-    case(1):
-      set_position(std::make_pair(get_position().first, 227));
-      if(event_name == "ENTER"){
-        action->execute("../data/2.level.dat");
-      }
-      break;
-    //CRÉDITOS
-    case(2):
-      set_position(std::make_pair(get_position().first, 280));
-      if(event_name == "ENTER"){
-        action->execute("../data/credits_scene.dat");
-      }
-      break;
-    //SAIR
-    case(3):
-      set_position(std::make_pair(get_position().first, 335));
-      if(event_name == "ENTER"){
+    switch (arrow_seletor) {
+    
+        //Initialize
+        case(0):
+            set_position(std::make_pair(get_position().first, 175));
+            
+            if (event_name == "ENTER") {
+                action->execute("../data/1.level.dat");
+            }
+            break;
+        
+        //Instructions
+        case(1):
+            set_position(std::make_pair(get_position().first, 227));
+            
+            if (event_name == "ENTER") {
+                action->execute("../data/2.level.dat");
+            }
+            break;
 
-      }
-      break;
-    default:
-      break;
-  }
-
+        //Credits
+        case(2):
+            set_position(std::make_pair(get_position().first, 280));
+            
+            if (event_name == "ENTER") {
+                action->execute("../data/credits_scene.dat");
+            }
+            break;
+        
+        //Exit
+        case(3):
+            set_position(std::make_pair(get_position().first, 335));
+            
+            if (event_name == "ENTER") {
+            }
+            break;
+        
+        default:
+            break;
+    }
 }
 
-void SelectArrow::update_state(){
-
+void SelectArrow::update_state() {
 }
