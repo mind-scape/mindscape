78fd986bfb4e0678c42c6225ef4985ac701c73e7
Bruna Pinos <bruna.pinos@gmail.com>
2017-09-08 19:14:28 -0300

    [COMMENT] Applies comments in goop.cpp and goop.hpp

--- include/goop.hpp
+++ include/goop.hpp
@@ -1,3 +1,13 @@
+/** 
+ * @file goop.hpp
+ * @brief Purpose: Contains methods to game class' management.
+ * 
+ * MIT License
+ * Copyright (c) 2017 MindScape
+ *
+ * https://github.com/TecProg2017-2/mindscape/blob/master/LICENSE.md
+ */
+
 #ifndef GOOP_H
 #define GOOP_H
 
@@ -15,32 +25,40 @@
 #include <string>
 
 namespace mindscape {
-  class Goop : public Enemy {
-    private:
+    
+    /** 
+     * @brief A Goop class.
+     * 
+     * The class Goop has attributes and methods for the goop of the Clow 
+     * character.
+     */ 
+    class Goop : public Enemy {
+        private:
 
-      void initialize_state_map();
-      void initialize_hitboxes();
-      void initialize_animations();
-      void initialize_as_physicable();
-      engine::Animation *create_animation(
-        std::string path,
-        int sprite_lines,
-        int sprite_columns,
-        double duration,
-        std::string direction
-      );
+            void initialize_state_map();
+            void initialize_hitboxes();
+            void initialize_animations();
+            void initialize_as_physicable();
+            engine::Animation *create_animation(
+                std::string path,
+                int sprite_lines,
+                int sprite_columns,
+                double duration,
+                std::string direction
+            );
 
-    public:
-      Goop(
-        std::string name,
-        std::pair<int, int> position,
-        int priority
-      );
-      ~Goop(){};
+        public:
+            Goop(
+                std::string name,
+                std::pair<int, int> position,
+                int priority
+            );
+            ~Goop(){};
 
-      void on_event(GameEvent);
-      void on_collision(engine::GameObject*, engine::Hitbox*, engine::Hitbox*);
-  };
+            void on_event(GameEvent);
+            void on_collision(engine::GameObject*, engine::Hitbox*,
+                                engine::Hitbox*);
+    };
 
 }
 
