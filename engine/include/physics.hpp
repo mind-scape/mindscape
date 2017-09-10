816a8ecef17b7241c693725088db06d6d061a5c9
Victor Moura <victor_cmoura@hotmail.com>
2017-09-10 12:52:40 -0300

    [COMMENT] Apply comment in physics.cpp and physics.hpp

--- engine/include/physics.hpp
+++ engine/include/physics.hpp
@@ -1,3 +1,12 @@
+/**
+* @file physics.hpp
+* @brief Purpose: Contains the Physics class declaration.
+*
+* MIT License
+* Copyright (c) 2017 MindScape
+*
+* https://github.com/TecProg2017-2/mindscape/blob/master/LICENSE.md
+*/
 #ifndef PHYSICS_H
 #define PHYSICS_H
 
@@ -7,28 +16,34 @@
 #include "game_object.hpp"
 
 namespace engine {
-  class Physics {
+    /**
+    * @brief A Physics class.
+    *
+    * A class responsible to ensure physics handling.
+    *
+    */
+    class Physics {
     private:
-      Physics(){};
+        Physics(){};
 
-      static Physics *instance;
-      static std::vector<GameObject *> physicables;
-      static float gravity;
+        static Physics *instance;
+        static std::vector<GameObject *> physicables;
+        static float gravity;
 
-      std::pair<float, float> calculate_speed(std::pair<float, float>);
-      std::pair<float, float> calculate_position(std::pair<float, float>, std::pair<float, float>);
-      void update_speed(GameObject *);
-      void update_position(GameObject *);
-      void act_on(GameObject *);
+        std::pair<float, float> calculate_speed(std::pair<float, float>);
+        std::pair<float, float> calculate_position(std::pair<float, float>, std::pair<float, float>);
+        void update_speed(GameObject *);
+        void update_position(GameObject *);
+        void act_on(GameObject *);
       
     public:
-      static Physics *get_instance();
-      ~Physics(){};
+        static Physics *get_instance();
+        ~Physics(){};
 
-      void operator=(Physics const&) = delete;
-      void add_physicable(GameObject *);
-      float get_gravity();
-      void act();
+        void operator=(Physics const&) = delete;
+        void add_physicable(GameObject *);
+        float get_gravity();
+        void act();
   };
 }
 
--- engine/src/physics.cpp
+++ engine/src/physics.cpp
@@ -1,3 +1,12 @@
+/**
+* @file physics.cpp
+* @brief Purpose: Keep methods that are related to any physical action.
+*
+* MIT License
+* Copyright (c) 2017 MindScape
+*
+* https://github.com/TecProg2017-2/mindscape/blob/master/LICENSE.md
+*/
 #include "physics.hpp"
 
 using namespace engine;
@@ -6,6 +15,13 @@ float Physics::gravity = 1.0;
 Physics *Physics::instance = 0;
 std::vector<GameObject *> Physics::physicables = {};
 
+/**
+* @brief Creates a Physics object instance.
+*
+* Physics object is required to ensure physical simulation
+*
+* @return Physics instance
+*/
 Physics *Physics::get_instance() {
 	if (!instance) {
 		instance = new Physics();
@@ -14,6 +30,14 @@ Physics *Physics::get_instance() {
 	return instance;
 }
 
+/**
+* @brief Calculates the speed of an object
+*
+* The objects of the game may require speed variations.
+*
+* @param std::pair with the current speed
+* @return std::pair with the new speed.
+*/
 std::pair<float, float> Physics::calculate_speed(std::pair<float, float> speed) {
 	std::pair<float, float> new_speed;
 	
@@ -22,7 +46,15 @@ std::pair<float, float> Physics::calculate_speed(std::pair<float, float> speed) 
 	
 	return new_speed;
 }
-
+/**
+* @brief Calculates the position of an object.
+*
+* With speed, the position of the game objects must change.
+*
+* @param std::pair with the current position of the object
+* @param std::pair with the current speed of the object
+* @return std::pair with the new position of the object.
+*/
 std::pair<float, float> Physics::calculate_position(std::pair<float, float> position, std::pair<float, float> speed) {
 	std::pair<float, float> new_position;
 	
@@ -31,14 +63,28 @@ std::pair<float, float> Physics::calculate_position(std::pair<float, float> posi
 	
 	return new_position;
 }
-
+/**
+* @brief Updates the speed of a game object.
+*
+* The speed must be set to the game object to ensure that the changes will be reflected on the gameplay
+*
+* @param the GameObject instance that will have its speed updated
+* @return void.
+*/
 void Physics::update_speed(GameObject *game_object) {
 	std::pair<float, float> old_speed = game_object->get_speed();
 	std::pair<float, float> new_speed = calculate_speed(old_speed);
 	
 	game_object->set_speed(new_speed);
 }
-
+/**
+* @brief Updates the position of a game object.
+*
+* The position must be set to the game object to ensure that the changes will be reflected on the gameplay
+*
+* @param the GameObject instance that will have its position updated
+* @return void.
+*/
 void Physics::update_position(GameObject *game_object) {
 	std::pair<float, float> speed = game_object->get_speed();
 	std::pair<float, float> old_position = game_object->get_position();
@@ -46,16 +92,36 @@ void Physics::update_position(GameObject *game_object) {
 	
 	game_object->set_position(new_position);
 }
-
+/**
+* @brief Updates speed and position of a game object.
+*
+* Since speed and position are mostly updated at the same time, the method does both at once
+*
+* @param the game object that will have its speed and position updated
+* @return void.
+*/
 void Physics::act_on(GameObject *game_object) {
 	update_speed(game_object);
 	update_position(game_object);
 }
-
+/**
+* @brief Ensures that the game object will be affected by the physics engine.
+*
+* The object must me physicable to be affected by physical simulation
+*
+* @param the game object that will be affected
+* @return void.
+*/
 void Physics::add_physicable(GameObject *game_object) {
 	physicables.push_back(game_object);
 }
-
+/**
+* @brief Ensures that the physical actions will affect all active objects.
+*
+* If the object is active, it must be affected by physical actions
+*
+* @return void.
+*/
 void Physics::act() {
 	for (auto game_object : physicables) {
 		if (game_object->is_active()) {
@@ -63,7 +129,13 @@ void Physics::act() {
 		}
 	}
 }
-
+/**
+* @brief Getter method for gravity.
+*
+* Makes gravity variable available outside the Engine
+*
+* @return float with the gravity value.
+*/
 float Physics::get_gravity() {
 	return gravity;
 }
