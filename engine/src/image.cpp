46817125f78594e51aa6b9b86edc9aa04b549f1e
Victor Moura <victor_cmoura@hotmail.com>
2017-10-23 12:50:34 -0200

    [DEFAULT BEHAVIOUR] Applies default behaviour in image.cpp

--- engine/src/image.cpp
+++ engine/src/image.cpp
@@ -37,6 +37,10 @@ bool Image::load() {
 			/* if the texture is null */
             ERROR("Unable to create texture");
         }
+        else {
+            /*Do nothing*/
+        }
+
         SDL_FreeSurface(loaded_surface);
     }
     else {
@@ -67,7 +71,11 @@ void Image::free() {
 
 		dimension_on_screen.first = 0;
 		dimension_on_screen.second = 0;
-	}
+    }
+    
+    else {
+        /*Do nothing*/
+    }
 }
 
 /**
