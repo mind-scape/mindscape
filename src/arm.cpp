61b27911d59065f64c87b7135da2146661bfa01a
Guilherme Lacerda <guiglacerda@gmail.com>
2017-10-30 02:35:37 -0200

    [Constants] Applies constants in arm

--- src/arm.cpp
+++ src/arm.cpp
@@ -76,6 +76,7 @@ void Arm::initialize_animations() {
 	);
 
 	add_animation("left_arm", left_arm);
+
 	set_actual_animation(right_arm);
 }
 
@@ -130,8 +131,9 @@ engine::Animation *Arm::create_animation(
  * @return void
  */
 void Arm::initialize_as_physicable() {
-	engine::Physics *physics = engine::Physics::get_instance();
+	engine::Physics *physics = engine::Physics::get_instance(); /**< Instance. Instance of the game */
 	physics->add_physicable(this);
+
 	collidable = true;
 }
 
@@ -143,7 +145,7 @@ void Arm::initialize_as_physicable() {
  * @return void
  */
 void Arm::initialize_hitboxes() {
-	engine::Game &game = engine::Game::get_instance();
+	engine::Game &game = engine::Game::get_instance(); /**< Instance. Instance of the game */
 
 	engine::Hitbox *arm_hitbox = new engine::Hitbox(
 			"arm_hitbox",
@@ -168,15 +170,19 @@ void Arm::initialize_hitboxes() {
  */
 void Arm::on_collision(engine::GameObject *other, engine::Hitbox *p_my_hitbox,
 					   engine::Hitbox *p_other_hitbox) {
-	Platform *platform = dynamic_cast<Platform *>(other);
-	engine::Hitbox *my_hitbox = dynamic_cast<engine::Hitbox *>(p_my_hitbox);
-	engine::Hitbox *other_hitbox = dynamic_cast<engine::Hitbox *>(p_other_hitbox);
+	Platform *platform = dynamic_cast<Platform *>(other); /**< Plataform. Plataform of the game */
+	engine::Hitbox *my_hitbox = dynamic_cast<engine::Hitbox *>(p_my_hitbox); /**< Hitbox. Arm hitbox */
+	engine::Hitbox *other_hitbox = dynamic_cast<engine::Hitbox *>(p_other_hitbox); /**< Hitbox. Girl hitbox */
 
 	if (get_speed_y() >= 0 && platform && my_hitbox->get_name() == "arm_hitbox") {
 		/* If arm is not falling */
 
-		set_speed_y(0.0);
-		set_position_y(other_hitbox->get_coordinates().second - 30);
+		const int movement_arm_axis_y = 30; /**< const int. Movement arm in pixel */
+		const float speed_arm = 0.0; /**< const float. Speed arm */
+
+		set_speed_y(speed_arm);
+		set_position_y(other_hitbox->get_coordinates()
+		 .second - movement_arm_axis_y);
 
 		states.set_state("Y_STATE", "ON_GROUND");
 
@@ -210,17 +216,24 @@ void Arm::initialize_state_map() {
  * @return void
  */
 void Arm::on_event(GameEvent game_event) {
-	std::string event_name = game_event.game_event_name;
+	std::string event_name = game_event.game_event_name; /**< String. name of game_event */
+	const int movement_arm = 10; /**< const int. Movemet of the arm in pixels */
 
 	if (event_name == "MOVE_LEFT" && !engine::GameObject::on_limit_of_level) {
+		/*
+		 * If arm is moving left.
+		 * Moves position 10 pixels to the right.
+		 */
 
-	/* If arm is moving left */
-		set_position_x(get_position_x() + 10); // Moves position 10 pixels to the right
+		set_position_x(get_position_x() + movement_arm);
 	}
 	else if (event_name == "MOVE_RIGHT" &&
 			!engine::GameObject::on_limit_of_level) {
-	/* If arm is moving right */
-		set_position_x(get_position_x() - 10); // Moves position 10 pixels to the left
+		/*
+		 * If arm is moving right.
+		 * Moves position 10 pixels to the left.
+		 */
+		set_position_x(get_position_x() - movement_arm);
 
 	}
 	else {
